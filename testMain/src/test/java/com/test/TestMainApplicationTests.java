c6070ca3a0e36eed03f131997544cff89ed63f02
wangban <yzbbanban@live.com>
2018-07-13 11:14:57 +0800

    添加gson

---
+++ testMain/src/test/java/com/test/TestMainApplicationTests.java
@@ -0,0 +1,30 @@
+package com.test;
+
+import com.test.yojson.Gson;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestMainApplicationTests {
+
+    @Test
+    public void contextLoads() {
+        Gson gson = new Gson();
+
+        String json = "{\n" +
+                "\"username\":\"ban\",\n" +
+                "\"password\":\"111\",\n" +
+                "\"age\":\"11\",\n" +
+                "\"school\":\"high\"\n" +
+                "}";
+        User user = gson.fromJson(json, User.class);
+        System.out.println("==================> user: " + user);
+
+
+    }
+
+}
